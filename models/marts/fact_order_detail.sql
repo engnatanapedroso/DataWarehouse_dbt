66bcbdfd28311f9f783953890017bfc638d8badb
engnatanapedroso@gmail.com <engnatanapedroso@gmail.com>
2022-01-22 00:25:36 +0000

    final

--- models/marts/fact_order_detail.sql
+++ models/marts/fact_order_detail.sql
@@ -26,7 +26,6 @@ with
             , credit_card.creditcard_sk
             , orders.orderdate
             , orders.status
-            , orders.totaldue
         from {{ref('stg_sales_order_header')}} as orders
         left join customer on orders.customerid = customer.customerid
         left join credit_card on orders.creditcardid = credit_card.creditcardid
@@ -37,6 +36,7 @@ with
             , product.product_sk
             , order_detail.orderqty
             , order_detail.unitprice
+            , order_detail.unitpricediscount
         from {{ref('stg_sales_order_detail')}} as order_detail
         left join product on order_detail.productid = product.productid
     )
@@ -47,10 +47,11 @@ with
             , order_detail_with_sk.product_sk as product_fk
             , order_with_sk.creditcard_sk as creditcard_fk
             , order_detail_with_sk.orderqty
-            , order_detail_with_sk.unitprice
             , order_with_sk.orderdate
             , order_with_sk.status
-            , order_with_sk.totaldue
+            , order_detail_with_sk.unitprice
+            , order_detail_with_sk.unitpricediscount
+
         from order_with_sk
         left join order_detail_with_sk on order_with_sk.salesorderid = order_detail_with_sk.salesorderid
     )
